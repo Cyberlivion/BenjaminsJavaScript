5a20ffa04f9bd90cabe6e35253d7d49134845892
Cyberlivion <72869277+Cyberlivion@users.noreply.github.com>
2022-02-08 10:24:52 +0100

    Add files via upload

---
+++ sidajs/js/domjs.js
@@ -0,0 +1,36 @@
+"use strict";
+varimageCounter = 1;
+showDiv(imageCounter);
+function changeImage(m) {
+
+showDiv(imageCounter= imageCounter+ m);
+
+}
+
+function showDiv(n) {
+
+vari;
+
+varimageArray =document.getElementsByClassName("mySlides");
+
+if(n > imageArray .lenght){
+
+imageCounter= 1;
+
+}
+
+if(n < 1){
+
+imageCounter= imageArray.length;
+
+}
+
+for(i = 0;i < imageArray.length;i++){
+
+imageArray[i].style.display= "none";
+
+}
+
+imageArray[imageCounter- 1].style.display= "block";
+
+}
